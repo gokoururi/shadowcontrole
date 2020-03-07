3e2ca308674e3ff217c61dc6fd0f20728f2c4935
gokoururi <asatier@gmail.com>
2020-03-07 21:29:08 +0100

    WIP

--- js/character.js
+++ js/character.js
@@ -14,17 +14,11 @@ module.exports = class Character {
     read() {
         var rawdata = fs.readFileSync(this.file);
         this.character = JSON.parse(rawdata);
-    }
 
-    write(data) {
-        fs.writeFileSync(this.file, data)
-    }
-
-    attributes() {
         var body = this.character.attributeGroups.body.attributes;
         var mind = this.character.attributeGroups.mind.attributes;
         var special = this.character.attributeGroups.special.attributes;
-        return {
+        this.attributes = {
             strength: parseInt(body.strength.value),
             reaction: parseInt(body.reaction.value),
             constitution: parseInt(body.constitution.value),
@@ -36,18 +30,29 @@ module.exports = class Character {
             essence: parseInt(special.essence.value),
             edge: parseInt(special.edge.value)
         }
-    }
 
-    limits() {
-        var a = this.attributes();
-        var limitBody = Math.ceil( ((a.strength*2)+a.constitution+a.reaction)/3 )
-        var limitMind = Math.ceil( ((a.logic*2)+a.intuition+a.willpower)/3 )
-        var limitSocial = Math.ceil( ((a.charisma*2)+a.willpower+a.essence)/3 )
-        return {
-            body: limitBody,
-            mind: limitMind,
-            social: limitSocial
+        var a = this.attributes;
+        this.limits = {
+            body: Math.ceil( ((a.strength*2)+a.constitution+a.reaction)/3 ),
+            mind: Math.ceil( ((a.logic*2)+a.intuition+a.willpower)/3 ),
+            social: Math.ceil( ((a.charisma*2)+a.willpower+a.essence)/3 )
         }
 
+        this.initiative = a.reaction + a.intuition + " + 1w6";
+        this.health = {
+            body: Math.ceil( 8 + (a.constitution/2)),
+            mind: Math.ceil( 8 + (a.willpower/2)),
+            overflow: a.constitution
+
+        }
+
+        this.status = {
+            
+        }
+    }
+
+    write(data) {
+        var stringifiedData = JSON.stringify(data, null, 2);
+        fs.writeFileSync(this.file, stringifiedData)
     }
 }
--- js/statblock.js
+++ js/statblock.js
@@ -60,8 +60,12 @@ function sheetRowHeading(heading) {
     return innerHTML;
 }
 
-function sheetRow(key, display, value, buttons=true) {
+function sheetRow(key, display, value, buttons=true, dataset={}) {
     let innerHTML = "";
+    var htmlData = '';
+    for (let [key, value] of Object.entries(dataset)) {
+        htmlData += ` data-${key}="${value}"`
+    }
     let colspan=2
     if ( buttons ) {
         colspan=0
@@ -71,7 +75,7 @@ function sheetRow(key, display, value, buttons=true) {
     innerHTML += `  <th>${display}</th>`
     innerHTML += `  <td class='value' colspan="${colspan}">${value}</td>`;
     if ( buttons ) {
-        innerHTML += '  <td><button class="edithidden small">-</button><button class="edithidden small">+</button></td>';
+        innerHTML += `  <td><button class="small" ${htmlData}>-</button><button class="small" ${htmlData}>+</button></td>`;
     }
     innerHTML += '</tr>';
     return innerHTML;
@@ -80,7 +84,8 @@ function sheetRow(key, display, value, buttons=true) {
 function print(char, elem) {
     const character = char.get();
     const element = document.querySelector(elem);
-    var innerHTML = '<table>';
+    var innerHTML = '';
+    innerHTML += '<table>';
     innerHTML += sheetRowHeading("Details");
     var details = character.details;
     for (var [detailName, detail] of Object.entries(character.details)) {
@@ -101,19 +106,36 @@ function print(char, elem) {
     innerHTML += '</div></th></tr>';
     */
     innerHTML += sheetRowHeading("Limits");
-    innerHTML += sheetRow("x", "Körperlich", char.limits().body, false);
-    innerHTML += sheetRow("x", "Geistig", char.limits().mind, false);
-    innerHTML += sheetRow("x", "Sozial", char.limits().social, false);
+    innerHTML += sheetRow("x", "Körperlich", char.limits.body, false);
+    innerHTML += sheetRow("x", "Geistig", char.limits.mind, false);
+    innerHTML += sheetRow("x", "Sozial", char.limits.social, false);
     innerHTML += sheetRowHeading("Berechnungen");
-    innerHTML += sheetRow("x", "Initiative", 2, false);
+    innerHTML += sheetRow("x", "Initiative", char.initiative, false);
     innerHTML += sheetRow("x", "Heben/Tragen", 2, false);
     innerHTML += sheetRow("x", "Laufen/Rennen", 2, false);
     innerHTML += sheetRow("x", "Menschenk.", 2, false);
     innerHTML += sheetRow("x", "Selbstbeh.", 2, false);
     innerHTML += sheetRow("x", "Erinnrungsv.", 2, false);
+    innerHTML += sheetRowHeading("Status");
+    innerHTML += sheetRow("edgeStatus", "Used Edge", character.status.edge + "/" + char.attributes.edge, true, { 'type': 'edge' });
+    innerHTML += sheetRow("dmg-body-temp", "DMG (K)", character.status.damage.body.temp, true, { 'type': 'dmg', 'path1': 'body', 'path2': 'temp'});
+    innerHTML += sheetRow("dmg-body-perm", "Perm. DMG (K)", character.status.damage.body.perm, true, { 'type': 'dmg', 'path1': 'body', 'path2': 'perm'});
+    innerHTML += sheetRow("x", "Leben (K)", char.health.body, false);
+    innerHTML += sheetRow("dmg-mind-temp", "DMG (G)", character.status.damage.mind.temp, true, { 'type': 'dmg', 'path1': 'mind', 'path2': 'temp'});
+    innerHTML += sheetRow("dmg-mind-perm", "Perm. DMG (G)", character.status.damage.mind.perm, true, { 'type': 'dmg', 'path1': 'mind', 'path2': 'perm'});
+    innerHTML += sheetRow("x", "Leben (K)", char.health.mind, false);
+    innerHTML += sheetRow("x", "Überzähliger DMG", char.health.overflow, false);
     innerHTML += '</table>';
+;
     element.innerHTML = innerHTML;
 
+    // Individual edit buttons
+    var buttons = element.querySelectorAll('button.small')
+    for (var button of buttons) {
+        button.addEventListener('click', (event) => { manipulateStatus(char, element, event); })
+    }
+
+    /*
     element.querySelector('button#edit').addEventListener('click', () => { cs_enableEdit(element, true) })
     element.querySelector('button#cancel').addEventListener('click', () => { cs_enableEdit(element, false); print(char, elem) })
     element.querySelector('button#save').addEventListener('click', () => {
@@ -127,6 +149,47 @@ function print(char, elem) {
     for (var button of buttons) {
         button.addEventListener('click', (event) => { cs_manipulateAttribute(element, event) })
     }
+    */
+}
+
+function manipulateStatus(char, element, event) {
+    var data = char.get();
+    var type = event.target.dataset.type
+    var add = true;
+
+    if ( event.target.innerHTML === '-' ) {
+        add = false;
+    }
+
+    if (type == "edge") {
+        var value = data.status.edge;
+        var displayElem = element.querySelector('#edgeStatus .value');
+        console.log(value);
+        if ( add ) {
+            value++;
+        } else {
+            value--;
+        }
+        console.log(value);
+        displayElem.innerHTML = value + '/' + char.attributes.edge;
+        data.status.edge = value;
+    } else if ( type == 'dmg' ) {
+        var path1 = event.target.dataset.path1;
+        var path2 = event.target.dataset.path2;
+        console.log('damage damage');
+        var displayElem = element.querySelector(`#${type}-${path1}-${path2} .value`);
+        var value = data.status["damage"][path1][path2];
+        if ( add ) {
+            value++;
+        } else {
+            value--;
+        }
+        displayElem.innerHTML = value;
+        data.status["damage"][path1][path2] = value;
+        console.log(value);
+    }
+
+    char.write(data);
 }
 
 exports.print = print;
